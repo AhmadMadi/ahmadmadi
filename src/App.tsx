c436c58faa1940615de391a4099725c744d7b133
Ahmad Madi <madi96.at.work@gmail.com>
2023-04-22 18:24:04 +0300

    Revert and fix last build error

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Container, Box, styled } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { DarkModeSwitch } from "react-toggle-dark-mode";
