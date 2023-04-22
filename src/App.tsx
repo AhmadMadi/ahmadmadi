03c19a25166c71456c990845de5bbae5fa35244e
Ahmad Madi <madi96.at.work@gmail.com>
2023-04-22 15:07:12 +0300

    Fix type err for styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { Container, Box } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { DarkModeSwitch } from "react-toggle-dark-mode";
 import CssBaseLine from "@mui/material/CssBaseline";
 import About from "./pages/about";
-import Projects from "./pages/projects";
+// import Projects from "./pages/projects";
 
 import "./App.css";
+
+type customCardShadowProp = {
+  "--card-shadow": string;
+};
 
 const App = () => {
   const [isDarkTheme, setIsDarkTheme] = useState(false);
   const theme = createTheme({
     palette: { mode: isDarkTheme ? "dark" : "light" },
   });
+
+  const customStyle: React.CSSProperties & customCardShadowProp = {
+    "--card-shadow": isDarkTheme
+      ? "0px 5px 20px 1px rgba(0,12,145,0.3)"
+      : "0px 7px 20px -6px rgba(0, 146, 199, 0.4)",
+    height: "100%",
+  };
 
   const toggleTheme = () => {
     setIsDarkTheme(!isDarkTheme);
@@ -21,14 +32,7 @@ const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseLine />
-      <div
-        style={{
-          "--card-shadow": isDarkTheme
-            ? "0px 5px 20px 1px rgba(0,12,145,0.3)"
-            : "0px 7px 20px -6px rgba(0, 146, 199, 0.4)",
-          height: "100%",
-        }}
-      >
+      <div style={customStyle}>
         <Box
           display={"flex"}
           justifyContent={"end"}
